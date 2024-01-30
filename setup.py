4967292843f1b91900dfe49af8f9dee5e7d90bc7
HamzaHaline <hhaline9@gmail.com>
2024-01-30 08:26:02 +0100

    Pushed the setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name='quiz-game-yourname-yourtdnumber',
+    version='1.0',
+    author='Your Full Name or GitHub Username',
+    license='MIT',
+    long_description=open('README.md').read(),
+)
